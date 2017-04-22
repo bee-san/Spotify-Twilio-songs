1e1ef3b47fc1484d6370f65ae8fe0904b0afa81b
nattop <nathan@windisch.co.uk>
2017-04-22 22:05:57 +0100

    added file outputting and a quit command, also added shorthand for all commands

--- spootipy.py
+++ spootipy.py
@@ -3,6 +3,7 @@ import sys
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 import spotipy.util as util
+import json
 
 client_id='04ba7c46b6224c5f9e3721469ea10bb3'
 client_secret='43077ed6c459406c8bc4d984c7fec45e'
@@ -13,58 +14,66 @@ playlist='7rDiVWYOh2abdvBB6Zcxva'
 
 scope = 'playlist-modify-private user-library-read user-library-modify playlist-modify-public playlist-read-collaborative playlist-read-private'
 token = util.prompt_for_user_token(client_id, scope)
-
-#ccm = SpotifyClientCredentials()
-#sp = spotipy.Spotify(ccm=ccm)
 sp = spotipy.Spotify(auth=token)
-
-#sp.authenticate
 
 def req():
     if token:
         util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
         sp.trace = True
         sp.trace_out = True
-        print("args: get, set")
+        print("args: g(et), s(et), q(uit)")
         answer1 = input()
-        if answer1 == 'get':
-            print("args: usr, usrdet, playlist")
+        if answer1 == 'get' or answer1 == 'g':
+            print("args: u(ser), p(laylist), q(uit)")
             answer2 = input()
-            if answer2 == 'usr':
+            if answer2 == 'user' or answer2 == 'u':
                 sp.current_user()
-            if answer2 == 'usrdet':
-                sp.me()
-            if answer2 == 'playlist':
+                with open('data/user.info', 'r+') as userinfo:
+                    json.dump(sp.current_user(), userinfo, ensure_ascii=False)
+            if answer2 == 'playlist' or answer2 == 'p':
                 sp.user_playlist(user_id, playlist)
+                with open('data/playlist.info', 'r+') as playlistinfo:
+                    json.dump(sp.user_playlist(user_id, playlist), playlistinfo, ensure_ascii=False)
+            if answer2 == 'quit' or answer2 == 'q':
+                sys.exit()
             else:
                 print("reloading the program")
                 main()
 
-        if answer1 == 'set':
-            print("args: add, rem, pos")
+        if answer1 == 'set' or answer1 == 's':
+            print("args: (a)dd, (r)emove, (p)osition, q(uit)")
             answer2 = input()
-            if answer2 == 'add':
+            if answer2 == 'add' or answer2 == 'a':
                 print("track id?")
                 track_id = input()
                 track_id2 = [ track_id ]
                 sp.user_playlist_add_tracks(user_id, playlist, track_id2)
-            if answer2 == 'rem':
+                with open('data/addhist.info', 'r+') as addhistinfo:
+                    json.dump(sp.user_playlist_add_tracks(user_id, playlist, track_id2), userinfo, ensure_ascii=False)
+            if answer2 == 'remove' or answer2 == 'r':
                 print("please note that this will remove all occurences of the track!")
                 print("track id?")
                 track_id = input()
                 track_id2 = [ track_id ]
                 sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist, track_id2)
-            if answer2 == 'pos':
+                with open('data/remhist.info', 'r+') as remhistinfo:
+                    json.dump(sp.user_playlist_remove_all_occurrences_of_tracks(user_id, playlist, track_id2), userinfo, ensure_ascii=False)
+            if answer2 == 'position' or answer2 == 'p':
                 print("track id?")
                 track_id = input()
                 track_id2 = [ track_id ]
                 print("position?")
                 pos = input()
                 sp.user_playlist_add_tracks(user_id, playlist, track_id2, pos)
+                with open('data/poshist.info', 'r+') as poshistinfo:
+                    json.dump(sp.user_playlist_add_tracks(user_id, playlist, track_id2, pos), userinfo, ensure_ascii=False)
+            if answer2 == 'quit' or answer2 == 'q':
+                sys.exit()
             else:
                 print("reloading the program")
                 main()
-        
+        if answer1 == 'quit' or answer1 == 'q':
+                sys.exit()
         else:
             print("reloading the program")
             main()
