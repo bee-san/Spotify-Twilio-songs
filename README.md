c665085cfe4569a5aacc4dcf64d6def08e16c3c2
nattop <nathan@windisch.co.uk>
2017-04-23 12:22:08 +0100

    updated readme, fixed flask code (so that it compiles at least), maybe did some other things too

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # Spotify Twilio Songs
-This is a hackathon project made by @BrandonSkerritt51 and @Natfan to attempt to combine cell phone texting with Spotify to allow users to create a constant playlist on the fly. Thanks to Spotify.com for giving us access to development tools, and Twilio.com for giving us access to use free mobile phone numbers.
+This is a hackathon project made by [@BrandonSkerritt51](https://github.com/BrandonSkerritt51/) and [@Natfan](https://github.com/Natfan/) to attempt to combine cell phone texting with Spotify to allow users to create a constant playlist on the fly. Thanks to [Spotify.com](https://spotify.com/) for giving us access to development tools, and [Twilio.com](https://twilio.com/) for giving us access to use free mobile phone numbers.
 
 # Todo:
 * Learn Spotify's REST API and Twilio's API.
--- pls_work_twilio.py
+++ pls_work_twilio.py
@@ -5,26 +5,26 @@ app = Flask(__name__)
 
 @app.route("/sms", methods=['GET', 'POST'])
 def sms_receive():
-	"""Respond to incoming calls with a simple text message."""
-	# Start our TwiML response
-	resp = MessagingResponse()
+    """Respond to incoming calls with a simple text message."""
+    # Start our TwiML response
+    resp = MessagingResponse()
 
-	message = request.values.get("Body", None)
-	print(message)
+    message = request.values.get("Body", None)
+    print(message)
 
-	if "STOP" in message.upper():
+    if "STOP" in message.upper():
+        print("test1")
+    if "SKIP" in message.upper():
+        print("test2")
+    if "BIEBER" in message.upper():
+	# rickroll pls
+        print("test3")
 
-	if "SKIP" in message.upper():
+    # stop, skip etc
 
-	if "BIEBER" in message.upper():
-		# rickroll pls
+    resp.message("YOU HAVE BEEN HEARD!")
 
-
-# stop, skip etc
-
-	resp.message("YOU HAVE BEEN HEARD!")
-
-	return str(resp)
+    return str(resp)
 
 if __name__ == "__main__":
 	app.run(debug=True)
